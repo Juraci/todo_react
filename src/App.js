9843eae96b16e00e8ae4f8472b7606d1861df82a
Juraci Vieira <juraci.vieira@gmail.com>
2017-07-23 12:30:38 -0300

    tracking input change

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ class App extends Component {
         { id: 1, name: 'Learn JSX', isComplete: true },
         { id: 2, name: 'Build an Awesome App', isComplete: false },
         { id: 3, name: 'Ship It!', isComplete: false },
-      ]
+      ],
+      currentTodo: '',
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   get_todo(todo) {
     return <li key={ todo.id }><input className="check-box" defaultChecked={ todo.isComplete } type="checkbox"/> { todo.name }</li>
+  }
+
+  handleInputChange(e) {
+    this.setState({ currentTodo: e.target.value });
   }
 
   render() {
@@ -27,7 +34,7 @@ class App extends Component {
         </div>
         <div className="Todo-App">
           <form>
-            <input type="text"/>
+            <input type="text" onChange={ this.handleInputChange } value={ this.state.currentTodo }/>
           </form>
           <div className="Todo-List">
             <ul>
